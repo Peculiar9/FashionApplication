9aaff48c01bb48fff2f426484410f9791052380c
Peculiar9 <omobabson@gmail.com>
2020-05-06 00:24:41 +0100

    Add project files.

---
+++ FashionApp/FashionApp/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Lobster-Regular.ttf", Alias = "Lobster")]
+[assembly: ExportFont("PlayfairDisplay-Black.ttf", Alias = "PlayfairDisplay")]
+[assembly: ExportFont("PlayfairDisplay-Bold.ttf", Alias = "PlayfairDisplay")]
+[assembly: ExportFont("Roboto-Bold.ttf", Alias = "Roboto")]
+[assembly: ExportFont("Roboto-Light.ttf", Alias = "Roboto")]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias = "Roboto")]
+[assembly: ExportFont("Roboto-Thin.ttf", Alias = "Roboto")]
